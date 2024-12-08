27f4458f3e8fcb1cc6a0dfc09d618042941739da
PARADISE-PARADOX <whoamiparadise@gmail.com>
2024-12-08 21:55:21 +0800

    import

---
+++ backend/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <>
+      <header className="header flex flex-sb">
+        <div className="logo flex gap-2">
+          <h1>Admin Panel</h1>
+          <div className="headerham flex flex-center"></div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
